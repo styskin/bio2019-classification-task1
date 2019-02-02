2f4cd6b4d01651986d7a331087307d9f8f732386
Andrey Styskin <styskin@macbook-air-6.local>
2019-02-02 15:27:47 +0300

    Task 1

--- task1/main.cpp
+++ task1/main.cpp
@@ -7,9 +7,36 @@
 //
 
 #include <iostream>
+#include <fstream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    ifstream in;
+    in.open("tests.txt");
+    ofstream out;
+    out.open("output.txt");
+    int T;
+    in >> T;
+    for (int t = 0; t < T; ++t) {
+        double n, a, b;
+        in >> n >> a >> b;
+        for (int i = 1; i < 10000000; ++i) {
+            n = a * n - b * n * n;
+            if (n < 0) {
+                n = 0;
+                break;
+            }
+            if (n > 1e50) {
+                n = -1;
+                break;
+            }
+//            cout << n << endl;
+        }
+//        cout << "=======" << endl;
+        out << n << endl;
+    }
+    in.close();
+    out.close();
+
     return 0;
 }
