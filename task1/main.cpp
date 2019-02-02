d487f22a4db6a99f6f2fe6ac7087b8bee0627434
Andrey Styskin <styskin@macbook-air-6.local>
2019-02-02 20:37:14 +0300

    Task1 optimization was added

--- task1/main.cpp
+++ task1/main.cpp
@@ -8,6 +8,8 @@
 
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
+#include <math.h>
 using namespace std;
 
 int main(int argc, const char * argv[]) {
@@ -18,10 +20,14 @@ int main(int argc, const char * argv[]) {
     int T;
     in >> T;
     for (int t = 0; t < T; ++t) {
-        double n, a, b;
+        double n, nn, a, b;
         in >> n >> a >> b;
-        for (int i = 1; i < 10000000; ++i) {
-            n = a * n - b * n * n;
+        for (int i = 1; i < 100000000; ++i) {
+            nn = a * n - b * n * n;
+            if (fabs(nn - n) < 1e-12) {
+                break;
+            }
+            n = nn;
             if (n < 0) {
                 n = 0;
                 break;
@@ -30,9 +36,7 @@ int main(int argc, const char * argv[]) {
                 n = -1;
                 break;
             }
-//            cout << n << endl;
         }
-//        cout << "=======" << endl;
         out << n << endl;
     }
     in.close();
